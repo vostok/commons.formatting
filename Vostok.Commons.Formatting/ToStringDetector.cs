e406ca8f0a8e90f6cb5ce4ce7c2f548bcb444570
iloktionov <iloktionov@skbkontur.ru>
2018-09-26 19:11:52 +0500

    Imported code and tests from vostok.logging.formatting.

---
+++ Vostok.Commons.Formatting/ToStringDetector.cs
@@ -0,0 +1,36 @@
+using System;
+using JetBrains.Annotations;
+using Vostok.Commons.Collections;
+
+// ReSharper disable ConvertClosureToMethodGroup
+
+namespace Vostok.Commons.Formatting
+{
+    [PublicAPI]
+    internal static class ToStringDetector
+    {
+        private const int CacheCapacity = 1000;
+
+        private static readonly RecyclingBoundedCache<Type, bool> Cache =
+            new RecyclingBoundedCache<Type, bool>(CacheCapacity);
+
+        public static bool HasCustomToString(Type type) =>
+            Cache.Obtain(type, t => HasCustomToStringInternal(t));
+
+        private static bool HasCustomToStringInternal(Type type)
+        {
+            var toStringMethod = type.GetMethod("ToString", Array.Empty<Type>());
+            if (toStringMethod == null)
+                return false;
+
+            // (iloktionov): Reject anonymous types:
+            if (type.Name.StartsWith("<>"))
+                return false;
+
+            var declaringType = toStringMethod.DeclaringType;
+
+            return declaringType != typeof(object) &&
+                   declaringType != typeof(ValueType);
+        }
+    }
+}
