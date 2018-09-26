5fe7f6e5ba0f5dc9557b217347346b8a8f3d96f2
iloktionov <iloktionov@skbkontur.ru>
2018-09-26 17:05:07 +0500

    Initialized repository with launchpad.

---
+++ Vostok.Commons.Formatting/SampleClass.cs
@@ -0,0 +1,13 @@
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Formatting
+{
+    /// <summary>
+    /// A sample class.
+    /// </summary>
+    [PublicAPI]
+    internal class SampleClass
+    {
+        
+    }
+}
