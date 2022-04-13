5b4b8b355f50ffc6eb1fdd71583806152ef39336
Ilya Tzoop <ilyatzoop@gmail.com>
2022-04-13 15:54:10 +0500

    Added MessageTemplateEscaper.cs

---
+++ Vostok.Commons.Formatting/MessageTemplateEscaper.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Formatting
+{
+    /// <summary>
+    /// Escapes given template according to https://vostok.gitbook.io/logging/concepts/syntax/message-templates
+    /// </summary>
+    [PublicAPI]
+    internal class MessageTemplateEscaper
+    {
+        public static string Escape(string template)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var chr in template)
+            {
+                if (chr == '{' || chr == '}')
+                    stringBuilder.Append(chr);
+                stringBuilder.Append(chr);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
