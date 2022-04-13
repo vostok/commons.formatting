7ddab7541bb1fc64d5646c308275bf4bbbe7b4e4
Ilya Tzoop <ilyatzoop@gmail.com>
2022-04-13 15:54:10 +0500

    Added MessageTemplateEscaper.cs

---
+++ Vostok.Commons.Formatting.Tests/MessageTemplateEscaper_Tests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Vostok.Commons.Formatting.Tests
+{
+    [TestFixture]
+    public class MessageTemplateEscaper_Tests
+    {
+        [Test]
+        public void Should_EscapeBrackets_CasualUsage()
+        {
+            MessageTemplateEscaper.Escape
+                    ("Session establishment complete on server {10.217.9.47:2181}, sessionid = 0x5047bed84ab9a42, negotiated timeout = 10000")
+               .Should()
+               .Be("Session establishment complete on server {{10.217.9.47:2181}}, sessionid = 0x5047bed84ab9a42, negotiated timeout = 10000");
+        }
+    }
+}
