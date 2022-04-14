74203da59e21b2beec51df8072dc82bc152937eb
Ilya Tzoop <ilyatzoop@gmail.com>
2022-04-14 13:23:28 +0500

    Remove unnecessary allocations

--- Vostok.Commons.Formatting.Tests/MessageTemplateEscaper_Tests.cs
+++ Vostok.Commons.Formatting.Tests/MessageTemplateEscaper_Tests.cs
@@ -14,5 +14,13 @@ namespace Vostok.Commons.Formatting.Tests
                .Should()
                .Be("Session establishment complete on server {{10.217.9.47:2181}}, sessionid = 0x5047bed84ab9a42, negotiated timeout = 10000");
         }
+
+        [Test]
+        public void Should_NotAllocateUselessMemory_WhenThereIsNothingToEscape()
+        {
+            // ReSharper disable once ConvertToConstant.Local
+            var str = "casual string";
+            MessageTemplateEscaper.Escape(str).Should().BeSameAs(str);
+        }
     }
 }
